fdf8295d11d91ba86bfc60ae5a94e8a4c5a46c28
aalhommada <aalhommada@gmail.com>
2019-11-07 11:15:09 +0100

    deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 
@@ -11,7 +12,7 @@ connectDB();
 //init meddilware
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('api requiest'))
+
 
 
 // Define Routes
@@ -20,7 +21,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
+  // serve html file
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
